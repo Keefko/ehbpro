8f8b716785fe4c9ad48f3f0d3884b4d58b27c543
Dan Kohout <kohooutdan@gmail.com>
2020-07-23 21:42:25 +0200

    secure change

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,9 +13,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
-    <script src="{{ asset('js/ebh.js') }}"></script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" href="{{ secure_asset('style.css') }}">
+    <script src="{{ secure_asset('js/ebh.js') }}"></script>
+    <script src="{{ secure_asset('js/app.js') }}" defer></script>
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
     <script>
         var editor_config = {
