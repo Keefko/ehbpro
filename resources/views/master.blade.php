2b1927384648f4e7e70ef4251e5d89afe4f8994e
Dan Kohout <kohooutdan@gmail.com>
2020-08-06 22:10:55 +0200

    hover dropdown menu

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -58,6 +58,13 @@
             $(".hamburger").click(function () {
                $('.wrapper').toggleClass("toggle");
             });
+
+            $('#navbarDropdown').hover(function() {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn();
+            }, function() {
+                $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut();
+            });
+
         });
     </script>
 </head>
