191d894cb830fa132fe2b4c47cc6c5215683f3c5
Dan Kohout <kohooutdan@gmail.com>
2020-08-09 09:42:23 +0200

    home page fixes

--- resources/views/partials/_aditional.blade.php
+++ resources/views/partials/_aditional.blade.php
@@ -11,7 +11,12 @@
             <div class="col-lg-6">
                 <div class="footer-form grey">
                     <div class="row">
-                        <div class="col-lg-10" data-aos="fade-left">
+                        <?php $width = '<script></script>'; ?>
+                        @if($width < 992)
+                                <div class="col-lg-10" data-aos="fade-up">
+                        @else
+                            <div class="col-lg-10" data-aos="fade-left">
+                         @endif
                             <h1>{{$section->title}}</h1>
                             <h6>{{$section->sub_title}}</h6>
                             <p class="mt-4">{!! $section->text !!}</p>
