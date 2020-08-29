b734356013ee96217646279977a1b9513a3ffe2a
Dan Kohout <kohooutdan@gmail.com>
2020-08-29 12:12:29 +0200

    s

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,17 @@ Route::get('/', function () {
 });
 
 
+Route::group(['middleware' => config('menu.middleware')], function () {
+    //Route::get('wmenuindex', array('uses'=>'\Harimayco\Menu\Controllers\MenuController@wmenuindex'));
+    $path = rtrim(config('menu.route_path'));
+    Route::post($path . '/addcustommenu', array('as' => 'haddcustommenu', 'uses' => '\Harimayco\Menu\Controllers\MenuController@addcustommenu'));
+    Route::post($path . '/deleteitemmenu', array('as' => 'hdeleteitemmenu', 'uses' => '\Harimayco\Menu\Controllers\MenuController@deleteitemmenu'));
+    Route::post($path . '/deletemenug', array('as' => 'hdeletemenug', 'uses' => '\Harimayco\Menu\Controllers\MenuController@deletemenug'));
+    Route::post($path . '/createnewmenu', array('as' => 'hcreatenewmenu', 'uses' => '\Harimayco\Menu\Controllers\MenuController@createnewmenu'));
+    Route::post($path . '/generatemenucontrol', array('as' => 'hgeneratemenucontrol', 'uses' => '\Harimayco\Menu\Controllers\MenuController@generatemenucontrol'));
+    Route::post($path . '/updateitem', array('as' => 'hupdateitem', 'uses' => '\Harimayco\Menu\Controllers\MenuController@updateitem'));
+});
+
 
 Route::post('partials._contact', 'MailController@postContact');
 
