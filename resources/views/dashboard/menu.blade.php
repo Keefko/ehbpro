bc194035e1dea43f9f6527d5b1157133f6e26b60
Dan Kohout <kohooutdan@gmail.com>
2020-08-29 12:14:55 +0200

    s

--- resources/views/dashboard/menu.blade.php
+++ resources/views/dashboard/menu.blade.php
@@ -5,52 +5,17 @@
 @section('content')
     @if(\Illuminate\Support\Facades\Auth::check())
 
-        {!! Menu::render() !!}
-{{--        <div class="wrapper">
+
+        <div class="wrapper">
             @include('dashboard.partials._menu')
             <div class="main_container">
                 <h1 class="pt-4 pb-2"><b>Nastavenia menu</b></h1>
-                <a href="{{ url('dashboard/menu/create') }}" class="btn btn-custom">Pridať novú položku</a>
-                @include('dashboard.partials._messages')
                 <div class="item mt-2">
                     <div class="row">
-
-                            <table class="table">
-                                <thead class="thead-light">
-                                <tr>
-                                    <th scope="col">Title</th>
-                                    <th scope="col">Url</th>
-                                    <th scope="col"></th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($menu as $item)
-                                        <tr>
-                                            <th scope="row"><a href="{{url('menu/'. $item->id . '/edit')}}">{{ $item->text}}</a></th>
-                                            <td>{{$item->url}}</td>
-                                            <td>
-                                                @csrf()
-                                                {!! \Collective\Html\FormFacade::open(['action' => ['MenuController@destroy', $item->id], 'method' => 'DELETE']) !!}
-                                                {{\Collective\Html\FormFacade::submit('Vymazať', ['class' => 'btn btn-custom mt-2'])}}
-                                                {!! \Collective\Html\FormFacade::close() !!}
-                                            </td>
-                                        </tr>
-                                        @if(count($item->submenus) > 0)
-                                            @foreach($item->submenus as $submenu)
-                                                <tr>
-                                                    <td></td>
-                                                    <td>{{$submenu->title}}</td>
-                                                    <td>{{$submenu->url}}</td>
-                                                </tr>
-                                            @endforeach
-                                        @endif
-
-                                    @endforeach
-                                </tbody>
-                            </table>
+                        {!! \Harimayco\Menu\Facades\Menu::render() !!}
                     </div>
                 </div>
-            </div>
+            </div>--}}
         </div>--}}
             @else
                 <script>window.location = "/";</script>
@@ -58,6 +23,6 @@
 @endsection
 
 @push('scripts')
-    {!! Menu::scripts() !!}
+    {!! \Harimayco\Menu\Facades\Menu::scripts() !!}
 @endpush
 </body>
