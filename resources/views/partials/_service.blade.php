96434e152f592a4e62053b1c972ad187da3fec7c
Dan Kohout <kohooutdan@gmail.com>
2020-09-12 07:43:04 +0200

    services 5 items design

--- resources/views/partials/_service.blade.php
+++ resources/views/partials/_service.blade.php
@@ -38,6 +38,48 @@
                             <h3>{{$item->title}}</h3>
                             <p>{!! $item->text !!}</p>
                         </div>
+                    @elseif(count($service) % 5 == 0){
+                        @if($count < 3)
+                            <div class="col-md-6 col-sm-6 col-xs-12 service-box" data-aos="flip-up">
+                                <div class="row">
+                                    <div class="col-md-6 service-image">
+                                        <img src="{{asset('img/service/'.$item->img)}}">
+                                    </div>
+                                    <div class="col-md-6 service-two">
+                                        <h2>0{{ $count }}</h2>
+
+                                        <?php $count++ ?>
+
+                                        <h3>{{$item->title}}</h3>
+                                        <p>{!! $item->text !!}</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        @else
+                            <div class="col-md-4 col-sm-6 col-xs-12 single-box" data-aos="flip-up">
+
+                                <h2>0{{ $count }}</h2>
+
+                                <style>
+                                    #service .services .single-box:nth-child({{$count}} ){
+                                        background: url({{asset('img/service/'.$item->img)}}) no-repeat center;
+                                        background-size:cover;
+                                        height: 350px;
+                                    }
+
+                                    #service .services .single-box:hover{
+                                        background: #e3e3e3;
+                                    }
+
+                                </style>
+
+                                <?php $count++ ?>
+                                <h3>{{$item->title}}</h3>
+                                <p>{!! $item->text !!}</p>
+                            </div>
+                        @endif
+                    }
                     @elseif(count($service) % 2 == 0)
                         <div class="col-md-6 col-sm-6 col-xs-12 service-box" data-aos="flip-up">
                             <div class="row">
