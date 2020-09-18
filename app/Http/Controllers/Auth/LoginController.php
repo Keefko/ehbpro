5d202c5cab520c21d56a2ddfe46f3de1e301feba
Keefko <58232282+Keefko@users.noreply.github.com>
2020-09-18 15:22:18 +0200

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,6 +27,8 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/dashboard';
+    protected $redirectAfterLogout = '/';
+    
     /**
      * Create a new controller instance.
      *
